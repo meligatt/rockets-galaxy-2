761f8d4df684e03615f455a22004253db191819a
Daniela Gattoni <daniela.gattoni@localz.co>
2018-10-20 13:08:41 +1100

    update: adding fake data object

---
+++ public/data.js
@@ -0,0 +1,41 @@
+const data = [
+  {
+    date: 'October 26, 2018',
+    launchWindow: '4:00 a.m. -- 5:30 a.m. Eastern'
+    mission: 'ICON (Ionospheric Connection Explorer)'
+    description: 'The Ionospheric Connection Explorer will study the frontier of space: the dynamic zone high in our atmosphere where Earth weather and space weather meet. ICON will launch from Cape Canaveral Air Force Station in Florida aboard an Northrop Grumman Pegasus XL rocket.',
+    crew: [
+      
+    ]
+  },
+  {
+    date: 'November 15, 2018 - 4:49 a.m. Eastern',
+    launchWindow: 'November 15, 2018 - 4:49 a.m. Eastern'
+    mission: ' Northrop Grumman Resupply Mission to Space Station (CRS-10)'
+    description: 'Northrop Grumman\'s tenth contracted commercial resupply services mission, launching aboard an Antares rocket from Wallops Flight Facility in Virginia, will deliver several tons of cargo including crew supplies and science experiments to the International Space Station.',
+    crew: [
+      
+    ]
+  },
+  {
+    date: 'November 26, 2018',
+    launchWindow: '4:00 a.m. -- 5:30 a.m. Eastern'
+    mission: 'ICON (Ionospheric Connection Explorer)'
+    description: 'The Ionospheric Connection Explorer will study the frontier of space: the dynamic zone high in our atmosphere where Earth weather and space weather meet. ICON will launch from Cape Canaveral Air Force Station in Florida aboard an Northrop Grumman Pegasus XL rocket.',
+    crew: [
+      
+    ]
+  },
+  {
+    date: 'InSight Landing on Mars',
+    launchWindow: '4:00 a.m. -- 5:30 a.m. Eastern'
+    mission: 'ICON (Ionospheric Connection Explorer)'
+    description: 'The Ionospheric Connection Explorer will study the frontier of space: the dynamic zone high in our atmosphere where Earth weather and space weather meet. ICON will launch from Cape Canaveral Air Force Station in Florida aboard an Northrop Grumman Pegasus XL rocket.',
+    crew: [
+      
+    ]
+  },
+
+]
+
+export default data;
