2214de3ca97cab8db449b45b67e91b121cf5ca62
melissa gattoni <meligatt@gmail.com>
2018-10-20 22:59:24 +1100

    Update Footer, Nav, App components, Add Artwork folder for svg components

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__container">
         <div className="footer__author">
-          <span>space apps 2018 by Team Wangalen</span>
+          <span>NASA Space Apps hackathon 2018</span>
         </div>
         <ul className="footer__link-list">
           <li className="footer__list-item">
-            link 1
+            <a href="https://2018.spaceappschallenge.org/challenges/can-you-build/when-next-rocket-launch/teams/wangalen/stream" rel='noopener noreferrer' target="_blank">Team Wangalen</a>
           </li>
           <li className="footer__list-item">
-            link 2
+            <a href="https://twitter.com/danigattoni_" rel='noopener noreferrer' target="_blank"> Daniela G.</a>
           </li>
           <li className="footer__list-item">
-            <a href="https://twitter.com/meligatt" rel='noopener noreferrer' target="_blank">Author</a>
+            <a href="https://twitter.com/meligatt" rel='noopener noreferrer' target="_blank">Melissa G.</a>
           </li>
         </ul>
       </div>
