276161b8fbce97b6d0ffb11b71be4c14cff493a8
melissa gattoni <meligatt@gmail.com>
2018-10-20 20:28:18 +1100

    Add component Widgets

--- containers/App.js
+++ containers/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Nav from './../components/Nav/index.jsx';
 import Main from './../components/Main/index.jsx';
 import Footer from './../components/Footer/index.jsx';
+import WidgetExpedition from './../components/WidgetExpedition/index.jsx';
+import WidgetCrew from './../components/WidgetCrew/index.jsx';
 import myData from 'data/data.js';
 
 class App extends Component {
@@ -10,51 +12,32 @@ class App extends Component {
     super();
     this.state = {
       data: [],
+      isLoading: true
     };
   }
   componentDidMount() {
-    console.log("componentDidMount!");
-    console.log("peace among worlds!");
-    console.log("another log in componentDidMount!");
-    console.log('myData: ', myData);
+    this.setState({
+      data: myData[3],
+      isLoading: false
+    });
   }
 
   render() {
+    const { mission, description, crew } = this.state.data;
+    const { isLoading } = this.state;
+    console.log(this.state.data);
+
     return (
       <div className="app-view">
         <div className="app-view__container">
           <Nav />
           <Main>
-            <div>
-              Hello Rockets of the galaxy Chrome extension!
-              <p>
-                <strong>
-                  practicing git commands!
-                  practicing git commands!
-                  practicing git commands!
-                  practicing git commands!
-                  practicing git commands!
-                  practicing git commands!
-                  practicing git commands!
-                  practicing git commands!
-                </strong>
-              </p>
-              <p>
-                Hello Rockets of the galaxy Chrome extension!
-              </p>
-              <p>
-                Hello Rockets of the galaxy Chrome extension!
-              </p>
-              <p>
-                Hello Rockets of the galaxy Chrome extension!
-              </p>
-              <p>
-                Hello Rockets of the galaxy Chrome extension!
-              </p>
-              <p>
-                Hello Rockets of the galaxy Chrome extension!
-              </p>
-            </div>
+            <WidgetExpedition
+              mission={mission}
+              description={description} />
+            {
+              !isLoading && <WidgetCrew crew={crew} />
+            }
           </Main>
         </div>
         <Footer />
