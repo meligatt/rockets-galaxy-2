cda09cf2a0d0133dd594909815de10d588942268
Daniela Gattoni <daniela.gattoni@localz.co>
2018-10-20 13:41:32 +1100

    adds basic json data from NASA launches and landings website

--- public/data.js
+++ public/data.js
@@ -1,8 +1,8 @@
 const data = [
   {
-    date: 'October 26, 2018',
-    launchWindow: '4:00 a.m. -- 5:30 a.m. Eastern'
-    mission: 'ICON (Ionospheric Connection Explorer)'
+    date: 'October 26, 2018',,
+    launchWindow: '4:00 a.m. -- 5:30 a.m. Eastern',
+    mission: 'ICON (Ionospheric Connection Explorer)',
     description: 'The Ionospheric Connection Explorer will study the frontier of space: the dynamic zone high in our atmosphere where Earth weather and space weather meet. ICON will launch from Cape Canaveral Air Force Station in Florida aboard an Northrop Grumman Pegasus XL rocket.',
     crew: [
       
@@ -10,8 +10,8 @@ const data = [
   },
   {
     date: 'November 15, 2018 - 4:49 a.m. Eastern',
-    launchWindow: 'November 15, 2018 - 4:49 a.m. Eastern'
-    mission: ' Northrop Grumman Resupply Mission to Space Station (CRS-10)'
+    launchWindow: '',
+    mission: ' Northrop Grumman Resupply Mission to Space Station (CRS-10)',
     description: 'Northrop Grumman\'s tenth contracted commercial resupply services mission, launching aboard an Antares rocket from Wallops Flight Facility in Virginia, will deliver several tons of cargo including crew supplies and science experiments to the International Space Station.',
     crew: [
       
@@ -19,23 +19,40 @@ const data = [
   },
   {
     date: 'November 26, 2018',
-    launchWindow: '4:00 a.m. -- 5:30 a.m. Eastern'
-    mission: 'ICON (Ionospheric Connection Explorer)'
-    description: 'The Ionospheric Connection Explorer will study the frontier of space: the dynamic zone high in our atmosphere where Earth weather and space weather meet. ICON will launch from Cape Canaveral Air Force Station in Florida aboard an Northrop Grumman Pegasus XL rocket.',
+    launchWindow: '',
+    mission: 'InSight Landing on Mars',
+    description: 'The Entry, Descent and Landing phase is the final plunge of the Mars InSight Lander through the Martian atmosphere. It lasts about six minutes and delivers the lander safely to the surface of the Red Planet.',
     crew: [
       
     ]
   },
   {
-    date: 'InSight Landing on Mars',
-    launchWindow: '4:00 a.m. -- 5:30 a.m. Eastern'
-    mission: 'ICON (Ionospheric Connection Explorer)'
-    description: 'The Ionospheric Connection Explorer will study the frontier of space: the dynamic zone high in our atmosphere where Earth weather and space weather meet. ICON will launch from Cape Canaveral Air Force Station in Florida aboard an Northrop Grumman Pegasus XL rocket.',
+    date: 'December 13, 2018',
+    launchWindow: '',
+    mission: 'Expedition 57 Landing',
+    description: 'Astronauts Serena Auñón-Chancellor of NASA, Alexander Gerst of the European Space Agency (ESA), and cosmonaut Sergey Prokopyev of the Russian space agency Roscosmos, will undock their Soyuz spacecraft from the International Space Station and land in Kazakhstan.',
     crew: [
       
     ]
   },
-
+  {
+    date: 'December 20, 2018',
+    launchWindow: '',
+    mission: 'Expedition 58 Launch',
+    description: 'NASA astronaut Anne McClain, Canadian Space Agency astronaut David Saint-Jacques and Oleg Kononenko of the Russian space agency Roscosmos launch to the International Space Station aboard the Russian Soyuz spacecraft from the Baikonur Cosmodrome in Kazakhstan.',
+    crew: [
+      
+    ]
+  },
+  {
+    date: 'January',
+    launchWindow: '',
+    mission: 'Commercial Crew SpaceX Demonstration Mission 1 (Uncrewed)',
+    description: 'NASA and industry partners are targeting the return of human spaceflight from Florida’s Space Coast. These flight tests will prove the space systems meet NASA’s requirements for certification to carry astronauts to and from the International Space Station. For this uncrewed flight test, SpaceX\'s Crew Dragon spacecraft will launch on a Falcon 9 rocket from Space Launch Complex 39A at Kennedy Space Center.',
+    crew: [
+      
+    ]
+  },
 ]
 
 export default data;
